f921199d0f1c29e1938a80910a7c1d7ba73e4f65
ANIMESH SINGH <me.animesh22@gmail.com>
2021-12-21 23:52:44 +0530

    Add files via upload

---
+++ get_ascii_value.py
@@ -0,0 +1,16 @@
+from colorama import Fore, Back, Style
+print(Fore.CYAN + "\t\t********HELLO FRI3NDS :)********\t\t")
+print("\t\tLET U GET THE ASCII VALUE OF ANY WORD SO SMILE :))\t\t")
+ascii = input("\tENTER THE WORD : \t" + Fore.GREEN)
+#print("\t", ascii ,"\t")
+print("ASCII VALUE OF '" + ascii + "' IS : ",ord(ascii))
+    
+    
+    
+    
+        
+    
+
+    
+    
+    
